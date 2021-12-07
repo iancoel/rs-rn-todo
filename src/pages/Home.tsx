f40f48ef54c36a848cd9868a5c59c8250475e15e
Ian Peres Coelho <ian.coelho@MLZPC-1072.local>
2021-12-07 19:10:44 -0300

    minor adjustments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,15 +5,27 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+// PAREI NA TOGGLE TASK DONE COM A TASKLIST
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    //add new task
+    setTasks((prev) => [
+      ...prev,
+      {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false,
+      },
+    ]);
+    console.log('adicionou');
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    console.log(id);
   }
 
   function handleRemoveTask(id: number) {
@@ -26,18 +38,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: '#EBEBEB',
+  },
+});
